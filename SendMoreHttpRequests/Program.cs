05b781657e767041ed9b089700089bbd55d00c16
XSkeolan <fspiritlan@gmail.com>
2022-01-27 10:19:28 +0300

    Add Get HTTP-requests

--- SendMoreHttpRequests/Program.cs
+++ SendMoreHttpRequests/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SendMoreHttpRequests
+{
+    class Program
+    {
+        static readonly HttpClient httpClient = new HttpClient();
+        static readonly string url = "https://google.com";
+        static readonly List<Task<string>> tasks = new List<Task<string>>();
+
+        static async Task Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+                tasks.Add(httpClient.GetStringAsync(url));
+
+            Task.WaitAll(tasks.ToArray());
+
+            var data = new List<string>();
+            foreach (var task in tasks)
+                data.Add(await task);
+
+            foreach (var response in data)
+                Console.WriteLine(response);
+        }
+    }
+}

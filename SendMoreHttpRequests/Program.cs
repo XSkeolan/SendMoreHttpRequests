4cf8091569b525f304cdb2e9f4312e3ff7d8d2d5
XSkeolan <fspiritlan@gmail.com>
2022-01-31 11:56:42 +0300

    increased count tasks

--- SendMoreHttpRequests/Program.cs
+++ SendMoreHttpRequests/Program.cs
@@ -3,20 +3,20 @@ namespace SendMoreHttpRequests
 {
     class Program
     {
-        private static readonly HttpClient httpClient = new HttpClient();
         static void Main(string[] args)
         {
+            HttpClient httpClient = new HttpClient();
             ResponseStatistic statistic;
-            string url = "https://reqbin.com/echo";
-            int number = 1;
+            string url = "http://localhost/";
+            int number = 101;
             Console.WriteLine("Time start - {0}", DateTime.Now);
-            var tasks = new List<Task>();
+            List<Task> tasks = new List<Task>();
             statistic = new ResponseStatistic();
-            for (int i = 0; i < 60;i++)
+            for (int i = 0; i < 100; i++)
                 tasks.Add(Task.Run(() =>
                 {
                     List<Task> tasksRequest = new List<Task>();
-                    for (int j = 0; j < 10000; j++)
+                    for (int j = 0; j < 15000; j++)
                         tasksRequest.Add(httpClient.GetAsync(url).ContinueWith((antecedent) =>
                         {
                             try
@@ -31,97 +31,29 @@ namespace SendMoreHttpRequests
                                 else
                                     statistic.Collect(StatisticType.Failed);
                             }
-                            catch(Exception)
+                            catch (Exception)
                             {
                                 statistic.Collect(StatisticType.Failed);
                             }
                         }));
-                    Console.WriteLine("{0} of the 1000 tasks waiting...", number++);
                     Task.WaitAll(tasksRequest.ToArray());
-                    //foreach (var t in tasksRequest)
-                    //    Console.WriteLine("{0,10} {1,20}", t.Id, t.Status);
-                    Console.WriteLine("{0} of the 1000 tasks already ran 600 tasks!!!", --number);
+                    Console.WriteLine("{0} of 100 tasks completed 15000 tasks!!!", 100 - --number);
                     tasksRequest.Clear();
-
-                    //for (int j = 0; j < 600; j++)
-                    //{
-                    //    try
-                    //    {
-                    //        if ((await tasksRequest[j]).StatusCode == HttpStatusCode.OK)
-                    //            statistic.Collect(StatisticType.Ok);
-                    //        else
-                    //            statistic.Collect(StatisticType.Failed);
-                    //    }
-                    //    catch (Exception e)
-                    //    {
-                    //        statistic.Collect(StatisticType.Failed);
-                    //    }
-                    //}
-                    ////List<Task<HttpResponseMessage>> tasks = new List<Task<HttpResponseMessage>>();
-                    //for (int i = 0; i < 50000; i++)
-                    //{
-                    //    //tasks.Add(httpClient.GetAsync(url));
-                    //    try
-                    //    {
-                    //        if ((await httpClient.GetAsync(url)).StatusCode == HttpStatusCode.OK)
-                    //            statistic.Collect(StatisticType.Ok);
-                    //        else
-                    //            statistic.Collect(StatisticType.Failed);
-                    //    }
-                    //    catch (Exception e)
-                    //    {
-                    //        statistic.Collect(StatisticType.Failed);
-                    //    }
-                    //}
-                    ////foreach (var task in tasks)
-                    ////{
-
-                    ////}
-                    //Console.WriteLine("Task ended");
                 }));
             try
             {
                 Task.WaitAll(tasks.ToArray());
                 foreach (var t in tasks)
-                    Console.WriteLine("{0,10} {1,20}",
-                                      t.Id, t.Status);
-                Console.WriteLine(statistic.Total);
+                    Console.WriteLine("{0,10} {1,20}", t.Id, t.Status);
             }
             catch (AggregateException)
             {
                 Console.WriteLine("Status of tasks:\n");
-                Console.WriteLine("{0,10} {1,20}", "Task Id",
-                                  "Status");
+                Console.WriteLine("{0,10} {1,20}", "Task Id", "Status");
                 foreach (var t in tasks)
-                    Console.WriteLine("{0,10} {1,20}",
-                                      t.Id, t.Status);
+                    Console.WriteLine("{0,10} {1,20}", t.Id, t.Status);
             }
-        }
-
-        static async Task Send()
-        {
-            //string url = "http://webcode.me";
-            ////List<Task<HttpResponseMessage>> tasks = new List<Task<HttpResponseMessage>>();
-            //for (int i = 0; i < 50000; i++)
-            //{
-            //    //tasks.Add(httpClient.GetAsync(url));
-            //    try
-            //    {
-            //        if ((await httpClient.GetAsync(url)).StatusCode == HttpStatusCode.OK)
-            //            statistic.Collect(StatisticType.Ok);
-            //        else
-            //            statistic.Collect(StatisticType.Failed);
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        statistic.Collect(StatisticType.Failed);
-            //    }
-            //}
-            //foreach (var task in tasks)
-            //{
-
-            //}
-            Console.WriteLine("Task ended");
+            Console.WriteLine("Time end - {0}", DateTime.Now);
         }
     }
 }

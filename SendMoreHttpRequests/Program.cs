4c78a23bdc5a326d9ff50adbb20bb55bbdc36f45
XSkeolan <fspiritlan@gmail.com>
2022-01-31 13:08:40 +0300

    add cancellationToken

--- SendMoreHttpRequests/Program.cs
+++ SendMoreHttpRequests/Program.cs
@@ -8,12 +8,13 @@ namespace SendMoreHttpRequests
             HttpClient httpClient = new HttpClient();
             ResponseStatistic statistic;
             string url = "http://localhost/";
+            CancellationTokenSource cts = new CancellationTokenSource();
             int number = 101;
             Console.WriteLine("Time start - {0}", DateTime.Now);
             List<Task> tasks = new List<Task>();
             statistic = new ResponseStatistic();
             for (int i = 0; i < 100; i++)
-                tasks.Add(Task.Run(() =>
+                tasks.Add(Task.Run(new Action(() =>
                 {
                     List<Task> tasksRequest = new List<Task>();
                     for (int j = 0; j < 15000; j++)
@@ -39,7 +40,7 @@ namespace SendMoreHttpRequests
                     Task.WaitAll(tasksRequest.ToArray());
                     Console.WriteLine("{0} of 100 tasks completed 15000 tasks!!!", 100 - --number);
                     tasksRequest.Clear();
-                }));
+                }), cts.Token));
 
             Console.WriteLine("Input Q or ESC for quit from application");
             ConsoleKeyInfo k;
@@ -51,6 +52,7 @@ namespace SendMoreHttpRequests
                     case ConsoleKey.Escape:
                     case ConsoleKey.Q:
                         {
+                            cts.Cancel();
                             Environment.Exit(0);
                             break;
                         }
